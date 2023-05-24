f361351669ebd7db586cd3770463dcf8250e71c2
Corentin Taplin <taplincorentin@gmail.com>
2023-05-24 12:03:18 +0200

    created Book class, started construct function

---
+++ Book.php
@@ -0,0 +1,20 @@
+<?php
+    class Book{
+
+        //ATTRIBUTES
+        private string $_title;
+        private string $_author;
+        private int $_nbPages;
+        private int $_pubYear;
+
+
+
+        //METHODS
+
+        //construct method
+        function __construct(string $title, string $author, int $nbPages, int $pubYear){
+            $this->_title = $title;
+            $this
+        }
+    }
+?>
